0e955a114a182cce3a225a5fd77490dd4c5cd2a9
Susaana7 <sussanateye7@gmail.com>
2026-03-30 16:25:50 +0000

    add ripple effect

--- src/Components/GameResult.jsx
+++ src/Components/GameResult.jsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
 import Token from "./Token";
 
 export function GameResult({ userPick, housePick, gameResult, resetGame }) {
   if (!userPick) return null;
+
+  const userWins = gameResult === "You win!";
+  const houseWins = gameResult === "You lose!";
+
+  const winnerRipple = "shadow-winner transition-shadow duration-500";
 
   return (
     <div className="relative flex flex-col items-center w-full min-h-[60vh]">
       <div className="flex flex-row items-center justify-between w-full max-w-3xl mt-12 px-6">
         {/* PLAYER SIDE */}
         <div className="flex flex-col items-center gap-8">
-          <p className="tracking-widest font-bold text-2xl">YOU PICKED</p>
-          <Token
-            icon={userPick.icon}
-            color={userPick.color}
-            positionClass="static"
-          />
+          <p className="tracking-widest font-bold">YOU PICKED</p>
+
+          <div className={`rounded-full ${userWins ? winnerRipple : ""}`}>
+            <Token
+              icon={userPick.icon}
+              color={userPick.color}
+              positionClass="static"
+            />
+          </div>
         </div>
+
         {/* WIN/LOSS MESSAGE (Only shows if gameResult exists) */}
         {gameResult && (
           <div className="flex flex-col items-center gap-4 mx-8">
@@ -32,11 +40,13 @@ export function GameResult({ userPick, housePick, gameResult, resetGame }) {
         <div className="flex flex-col items-center gap-8">
           <p className="tracking-widest font-bold text-2xl">THE HOUSE PICKED</p>
           {housePick ? (
-            <Token
-              icon={housePick.icon}
-              color={housePick.color}
-              positionClass="static"
-            />
+            <div className={`rounded-full ${houseWins ? winnerRipple : ""}`}>
+              <Token
+                icon={housePick.icon}
+                color={housePick.color}
+                positionClass="static"
+              />
+            </div>
           ) : (
             <div className="w-32 h-32 bg-black/20 rounded-full" />
           )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        winner:
+          "0 0 0 40px rgba(255,255,255,0.05), 0 0 0 80px rgba(255,255,255,0.03), 0 0 0 130px rgba(255,255,255,0.02)",
+      },
+    },
   },
   plugins: [],
 };
